718cf91453549ed348eb8595006f74fb39694f09
Uyen-nt-Le <47649760+Uyen-nt-Le@users.noreply.github.com>
2019-10-22 22:12:34 -0700

    Update super_mario.py

--- super_mario.py
+++ super_mario.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 from enemy import Goomba
-from enemy import Koopa_Troopa
+from enemy import KoopaTroopa
+from enemy import Podoboo
 from pygame.sprite import Group
 from pygame.locals import *
 
@@ -13,9 +14,16 @@ def run_game():
     screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
     pygame.display.set_caption("Super Mario")
 
-    # Change these to groups of sprites!?
-    goomba = Goomba(850, 430)
-    koopa = Koopa_Troopa(500, 420)
+    # Sprites
+    koopa1 = KoopaTroopa(screen, 500, 420)
+    koopa2 = KoopaTroopa(screen, 400, 420, 300, 700)
+    goomba1 = Goomba(screen, 850, 430)
+    podoboo1 = Podoboo(screen, 350, 500)
+    allSprites = Group()
+    allSprites.add(koopa1)
+    allSprites.add(goomba1)
+    allSprites.add(podoboo1)
+    allSprites.add(koopa2)
 
     game_over = False
     while not game_over:
@@ -28,8 +36,14 @@ def run_game():
         pygame.draw.rect(screen, (0, 200, 0), (0, 450, 300, 50))  # ground
         pygame.draw.rect(screen, (0, 200, 0), (400, 450, 500, 50))  # ground
 
-        goomba.drawMe(screen)
-        koopa.drawMe(screen)
+        # goomba.drawMe(screen)
+        # koopa.drawMe(screen)
+        # goombas.update(screen)
+        #goombas.update()
+        #goombas.draw(screen)
+        # goombas.draw(screen)
+        allSprites.update()
+
 
         pygame.display.update()
 
