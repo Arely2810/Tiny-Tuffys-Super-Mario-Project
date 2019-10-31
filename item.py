357db13cd3dc90430a96f4c6163dac2705258c6d
gyee1996 <griffinyee@csu.fullerton.edu>
2019-10-30 23:54:46 -0700

    Add files via upload

---
+++ item.py
@@ -0,0 +1,154 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Item(Sprite):
+    def __init__(self):
+        self.count = 0
+        self.vel = 5
+        self.yvel = 5
+        self.popout_delay = 0
+
+        #placeholder image
+        self.image = pygame.image.load('images/super_mushroom.png')
+
+class Coin(Item):
+    coin_image = [pygame.image.load('images/coin1.png'), pygame.image.load('images/coin2.png'), pygame.image.load('images/coin3')]
+    def __init__(self, pos, screen):
+        super().__init__(pos, screen)
+        self.image = coin_image
+        self.pos = pos
+        self.screen = screen
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if popout_delay < 30:
+            self.y -= self.yvel
+            self.popout_delay += 1
+        elif popout_delay == 30:
+            self.y += self.yvel
+        elif self.y == self.pos[1]:
+            self.kill()
+
+
+
+
+
+class Mushroom(Item):
+    mushroom_image = pygame.image.load('images/super_mushroom.png')
+    def __init__(self, pos, screen):
+        super().__init__(pos, screen)
+        self.image = mushroom_image
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        self.falling = False
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if popout_delay < 32:
+            self.y -= 1
+        else:
+            self.normal_movement()
+        if self.y > 600:
+            self.kill()
+
+    def normal_movement(self):
+        self.x += self.vel
+        if falling:
+            self.y += self.yvel
+
+    def bumped(self):
+        self.vel *= -1
+        timer = 0
+        while timer < 32:
+            self.y +=1
+            timer += 1
+        self.falling = True
+
+class OneUp(Item):
+    one_up_image = pygame.image.load('images/one_up_shroom.png')
+    def __init__(self, pos, screen):
+        super().__init__(pos, screen)
+        self.image = one_up_image
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        self.falling = False
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if popout_delay < 32:
+            self.y -= 1
+        else:
+            self.normal_movement()
+        if self.y > 600:
+            self.kill()
+
+    def normal_movement(self):
+        self.x += self.vel
+        if falling:
+            self.y += self.yvel
+
+    def bumped(self):
+        self.vel *= -1
+        timer = 0
+        while timer < 32:
+            self.y += 1
+            timer += 1
+        self.falling = True
+
+class FireFlower(Item):
+    fire_flower_image = [pygame.image.load('images/fire_flower1.png'), pygame.image.load('images/fire_flower2.png'),pygame.image.load('images/fire_flower3.png'), pygame.image.load('images,fire_flower4.png')]
+    def __init__(self, pos, screen):
+        super(FireFlower, self).__init__()
+        self.image = fire_flower_image
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+class Star(Item):
+    star_image = [pygame.image.load('images/star1.png'), pygame.image.load('images/star2.png'),pygame.image.load('images/star3.png'), pygame.image.load('images/star4.png')]
+    def __init__(self, pos, screen):
+        super(Star, self).__init__()
+        self.image = star_image
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if popout_delay < 32:
+            self.y -= 1
+        else:
+            self.normal_movement()
+
+        if self.y > 600:
+            self.kill()
+
+    def normal_movement(self):
+        self.x += self.vel
+        self.y += self.yvel
+
