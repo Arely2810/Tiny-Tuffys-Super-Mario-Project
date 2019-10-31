7156ba01c8bc59335f713b669fbb7ae25c602f6c
Uyen-nt-Le <47649760+Uyen-nt-Le@users.noreply.github.com>
2019-10-31 02:23:25 -0700

    Update temp_settings.py

--- temp_settings.py
+++ temp_settings.py
@@ -19,6 +19,31 @@ class Settings:
         self.bg_x = 0
         self.bg_y = 0
 
+        # blocks
+        # Level 1
+        block_position1 = [(512, 288), (640, 288), (672, 288), (704, 288), (736, 288), (768, 288), (704, 160)]
+        block_type1 = [3, 1, 3, 1, 3, 1, 3]
+        block_items1 = [1, 0, 2, 0, 1, 0, 1]
+        pipe_position1 = [(896, 352), (1184, 320)]
+        pipe_size1 = [0, 1]
+        # Level 2
+        block_position2 = [(50, 50), (100, 50,), (400, 50)]
+        block_type2 = [3, 3, 4]
+        block_items2 = [2, 1, 1]
+
+        # index variables
+        self.current_level = 0
+        self.current_block = 0
+        self.current_pipe = 0
+
+        self.number_of_blocks = [7, 3]
+        self.number_of_pipes = [2]
+        self.pipe_positions = [pipe_position1]
+        self.pipe_sizes = [pipe_size1]
+        self.block_types = [block_type1, block_type2]
+        self.block_positions = [block_position1, block_position2]
+        self.block_items = [block_items1, block_items2]
+
     def bg_position(self):
         return self.bg_x, self.bg_y
 
