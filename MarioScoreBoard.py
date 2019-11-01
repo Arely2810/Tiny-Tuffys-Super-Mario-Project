243811660950efc19718ffb55beba5d75407e5f1
YasminTorres <torresyasmin21@csu.fullerton.edu>
2019-10-31 20:33:36 -0700

    Update MarioScoreBoard.py

--- MarioScoreBoard.py
+++ MarioScoreBoard.py
@@ -13,14 +13,17 @@ class ScoreBoard:
         self.coins = 0
         self.screen = screen
         self.screen_rect = screen.get_rect()
-        self.font_name = pygame.font.match_font('mario font 256')
+        # self.font_name = pygame.font.match_font('mario font 256')
         self.white = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+        self.center_x = 250
 
         self.setup_score()
-        self.setup_time()
+
+    ''' self.setup_time()
         self.setup_world()
         self.setup_coins()
-        self.setup_lives()
+        self.setup_lives()'''
 
     def enemy_killed(self, enemy_type):
         if enemy_type == 'turtle':
@@ -41,17 +44,20 @@ class ScoreBoard:
         elif not died:
             self.lives = self.lives
 
-    def timer_countdown(self, mariox):
-        while mariox != 14036:
+    def timer_countdown(self, mario_x):
+        while mario_x != 14036:
             self.time = self.time - 1
 
     def setup_score(self):
         score = self.score
         score_str = "Score: {}".format(score)
-        self.screen.draw.text((90, 50), score_str, self.white)
-        self.screen.update()
+        score_image = self.font.render(score_str, True, self.white)
 
-    def setup_time(self):
+        score_rect = score_image.get_rect()
+        score_rect.center = self.screen_rect.center
+        score_rect.top = score_rect.top
+
+    '''def setup_time(self):
         time = self.time
         time_str = "Time: {}".format(time)
         self.screen.draw.text((190, 50), time_str, self.white)
@@ -74,4 +80,4 @@ class ScoreBoard:
         lives = self.lives
         lives_str = "Lives: {}".format(lives)
         self.screen.draw.text((490, 50), lives_str, self.white)
-        self.screen.update()
+        self.screen.update()'''
