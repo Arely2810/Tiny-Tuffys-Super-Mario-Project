47eeac677d539bf2728549df098b3e9db036f3e8
gyee1996 <griffinyee@csu.fullerton.edu>
2019-11-01 00:46:25 -0700

    Add files via upload

---
+++ ground.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ground(Sprite):
+    def __init__(self, screen, pos, size):
+        super(Ground, self).__init__()
+        self.screen = screen
+        self.pos = pos
+        self.image = pygame.Surface((size), pygame.SRCALPHA)
+        self.image = self.image.convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.pos[0]
+        self.rect.y = self.pos[1]
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
