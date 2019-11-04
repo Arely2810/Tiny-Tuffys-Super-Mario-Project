1a1d54b259d888645a5f9b8d5040dca48bc9c33e
Uyen-nt-Le <47649760+Uyen-nt-Le@users.noreply.github.com>
2019-11-03 16:08:47 -0800

    Update game.py

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ from mario import Mario
 import game_functions as gf
 from settings import Settings
 from scoreboard import ScoreBoard
+from pole import Pole
 
 
 pygame.init()
@@ -24,24 +25,25 @@ mario = Mario(settings, display_screen)
 enemies = Group()
 blocks = Group()
 pipes = Group()
+poles = Group()
+flags = Group()
+
 fireball = Group()
 
-gf.create_block(settings, display_screen, blocks)
-gf.create_pipe(settings, display_screen, pipes)
-gf.create_enemy(settings, display_screen, enemies)
+# gf.create_block(settings, display_screen, blocks)
+# gf.create_pipe(settings, display_screen, pipes)
+# gf.create_enemy(settings, display_screen, enemies)
+gf.create_entities(settings, display_screen, blocks, pipes, enemies, poles, flags)
 
-# goom = Goomba(settings.display_screen, (800, 383), 1)
-# enemies.add(goom)
 
 # main loop
 while True:
-    # gf.check_events(mario)
     gf.mario_move(mario, settings, display_screen, fireball)
     display_screen.blit(settings.bg_image, settings.bg_position())
 
     gf.mario_in_range(mario, enemies)
-    gf.scroll_eveything_left(settings, mario, enemies, blocks, pipes)
-    gf.check_collision(settings, scoreboard, enemies, mario, blocks, pipes)
+    gf.scroll_eveything_left(settings, mario, enemies, blocks, pipes, poles, flags)
+    gf.check_collision(settings, scoreboard, enemies, mario, blocks, pipes, flags)
 
     scoreboard.update_screen()
 
@@ -51,9 +53,13 @@ while True:
     blocks.draw(display_screen)
     pipes.update()
     pipes.draw(display_screen)
+    poles.draw(display_screen)
+    poles.update()
+    flags.draw(display_screen)
+    flags.update()
 
     if mario.death:
-        gf.reset_game(display_screen, settings, mario, enemies, blocks, pipes)
+        gf.reset_game(display_screen, settings, mario, enemies, blocks, pipes, poles, flags)
 
     pygame.display.update()
     CLOCK.tick(settings.FPS)
